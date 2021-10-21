814210eb5c0318584d836b01bd8938fb434d2517
Moacir Torres <92885652+moacirtorres@users.noreply.github.com>
2021-10-21 20:35:29 -0300

    login-page-vue
    Initial commit

---
+++ src/views/LoginLayout/Login.vue
@@ -0,0 +1,66 @@
+<template>
+
+    <div id="app">
+        <div class="container">
+        <section class="form my-4 mx-5">
+            <div class="container container-style">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <img src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" class="img-fluid" alt="" height="300px" width="400px">
+                    </div>
+                    <div class="col-lg-7">
+                        <form action="">
+                            <div class="form-row">
+                                <div class="col-lg-7">
+                                    <input type="email" name="" id="">
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="col-lg-7">
+                                    <input type="password" name="" id="">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    </div>
+
+
+
+
+</template>
+
+
+
+<script>
+    export default {
+
+    }
+</script>
+
+
+<style scoped>
+
+*{
+    margin: 0;
+    padding: 0;
+}
+
+
+.container-style {
+    text-align: center;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgb(2,0,36);
+    margin: 0;
+    padding: 0;
+background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(247,129,9,1) 100%, rgba(0,212,255,1) 100%);
+}
+
+
+</style>
