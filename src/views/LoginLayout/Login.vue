05d2af78c6d4ed8181943077e05d72c374ed046c
Moacir Torres <92885652+moacirtorres@users.noreply.github.com>
2021-10-22 16:49:16 -0300

    Final version
    final version login app with vue cli just for training

--- src/views/LoginLayout/Login.vue
+++ src/views/LoginLayout/Login.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <div class="card mb-3 " style="max-width: 540px">
+    <div class="card mb-3 border-0 " style="width: 600px">
       <div class="row g-0 justify-content-center">
-        <div class="col-md-4">
-          <img src="https://picsum.photos/200/300" class="img-fluid rounded-start" alt="..." />
+        <div class="col-md-5">
+          <img src="https://pm1.narvii.com/6963/1d4ca65ea5f648a8cb790ac45e91b708ae1b4e0br1-1536-2048v2_hq.jpg" class="img-fluid rounded-start" alt="..." />
         </div>
-        <div class="col-md-8">
+        <div class="col-md-7">
           <div class="card-body">
-            <h1 class="card-title text-center">LOGIN</h1>
+            <h1 class="card-title text-center">Login</h1>
             <form action="">
                 <label for="">E-mail</label>
-                <input class="form-control" type="text">
+                <input class="form-control mt-2" type="text">
                 <label for="">Senha</label>
-                <input class="form-control" type="text">
+                <input class="form-control mt-2" type="text">
             </form>
+            <div class="d-flex text-center flex-column">
+              <button class="btn p-3 mt-4 button-style">Entrar</button>
+            </div>
           </div>
         </div>
       </div>
@@ -40,6 +43,11 @@ export default {};
     justify-content: center;
     background: rgb(240,3,3);
     background: linear-gradient(90deg, rgba(240,3,3,1) 14%, rgba(247,129,9,1) 96%);
+}
+
+.button-style {
+  background-color: tomato !important; 
+  color: white !important;
 }
 
 
